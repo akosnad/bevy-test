6970d34729b87ac8ac83de2b905f6992f83fa4a1
akosnad <akos@nadudvari.org>
2024-11-03 20:19:02 +0100

    feat(flake): use flake-parts

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     naersk.url = "github:nix-community/naersk";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
@@ -33,6 +29,7 @@
           nativeBuildInputs = with pkgs; [
             rustc
             cargo
+            clippy
             pkg-config
           ];
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
@@ -40,12 +37,7 @@
           naersk = pkgs.callPackage inputs.naersk { };
         in
         {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              inputs.fenix.overlays.default
-            ];
-          };
+          _module.args.pkgs = import inputs.nixpkgs { inherit system; };
 
           treefmt.config = {
             projectRootFile = "flake.nix";
@@ -68,44 +60,3 @@
         };
     };
 }
-
-/*
-  flake-utils.lib.eachSystem systems (system:
-      let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
-
-        naersk' = pkgs.callPackage naersk {};
-
-        buildInputs = with pkgs; [
-          udev alsa-lib vulkan-loader
-          xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr
-          libxkbcommon wayland
-        ];
-        nativeBuildInputs = with pkgs; [
-          rustc
-          cargo
-          pkg-config
-        ];
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-
-      in rec {
-        # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
-          inherit buildInputs nativeBuildInputs LD_LIBRARY_PATH;
-        };
-
-        # For `nix develop`:
-        devShell = pkgs.mkShell {
-          inherit buildInputs nativeBuildInputs LD_LIBRARY_PATH;
-        };
-
-        checks = {
-          default = defaultPackage;
-        };
-      }
-  );
-  }
-*/
